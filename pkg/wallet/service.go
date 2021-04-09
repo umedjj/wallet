7ef0f9f91a32671e3656de70de2114ef39ea27d6
umedkk <umedkk@go.alif.academy>
2021-04-09 20:10:58 +0300

    Добавлен фуекция FindAccountByID

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -1,2 +1,33 @@
 package wallet
 
+import(
+	"github.com/umedjj/wallet/pkg/types"
+	"errors"
+)
+
+type Service struct {
+	nextAccountID int64
+	accounts      []*types.Account
+	payments      []*types.Payment
+	}
+
+var ErrAccountNotFound =  errors.New("account not found")
+//s.RegisterAccount undefined (тип * Service не имеет поля или метода RegisterAccount)
+
+func (s *Service) FindAccountByID(accountID int64)(*types.Account, error)  {
+	var account *types.Account
+	for _, acc := range s.accounts {
+
+		if acc.ID == accountID {
+			account = acc
+			
+		}
+		
+	}
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+
+	return  account, nil
+}
+
