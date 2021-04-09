45a656538059be84d4af64c336eda63934ef1e2c
umedkk <umedkk@go.alif.academy>
2021-04-09 20:17:10 +0300

    Добавлен автотесты

---
+++ pkg/wallet/service_test.go
@@ -0,0 +1,29 @@
+package wallet
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/umedjj/wallet/pkg/types"
+)
+
+func TestService_FindAccountByID(t *testing.T) {
+	
+		service := Service{
+			accounts: []*types.Account{
+				{ ID: 10, Phone: "9929888881", Balance: 1000,},
+				{ ID: 15, Phone: "9929999991", Balance: 1500,},		
+			},
+		}
+	
+		expected := &types.Account{
+			ID: 10, Phone: "9929888881", Balance: 1000,	
+		}
+	
+		result, _ := service.FindAccountByID(10)
+	
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("Invalid Result: Excpected: %v, actual: %v ", expected, result)
+		}
+	}
+
