5e5697bef47376743b399160f0e9777c02db2f29
umedkk <umedkk@go.alif.academy>
2021-04-11 10:03:03 +0300

    v1.1.0

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -6,17 +6,50 @@ import(
 	"github.com/google/uuid"
 )
 
-type Service struct {
-	nextAccountID int64
-	accounts      []*types.Account
-	payments      []*types.Payment
+var ErrPhoneRegistered = errors.New("Phone already Registered")
+var ErrAmountMustBePositive= errors.New("Amount must be greater than 0")
+var ErrAccountNotFound= errors.New("Account not found")
+var ErrNotEnoughBalance = errors.New("Balance not enough")
+type Service struct{
+	nextAccountID	int64
+	accounts 		[]*types.Account
+	payments 		[]*types.Payment
+}
+
+func(s *Service)  RegisterAccount(phone types.Phone) (*types.Account, error) {
+	for _, account := range s.accounts {
+		if account.Phone==phone{
+			return nil, ErrPhoneRegistered
+		}
+	}
+	s.nextAccountID++
+	account :=	&types.Account{
+		ID:      s.nextAccountID,
+		Phone:   phone,
+		Balance: 0,
+	}
+	s.accounts=append(s.accounts, account)
+	return account, nil
+}
+
+func (s *Service) Deposit(accountID int64, amount types.Money) error {
+	if amount<= 0{
+		return ErrAmountMustBePositive
+	}
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID==accountID{
+			account=acc
+			break
+		}		
+	}
+	if account==nil{
+		return ErrAccountNotFound
 	}
 
-	var ErrPhoneRegistered = errors.New("Phone already Registered")
-	var ErrAmountMustBePositive= errors.New("Amount must be greater than 0")
-	var ErrAccountNotFound= errors.New("Account not found")
-	var ErrNotEnoughBalance = errors.New("Balance not enough")
-
+	account.Balance+=amount
+	return nil
+}
 
 func (s *Service) Pay(accountID int64, amount types.Money, category types.PaymentCategory) (*types.Payment, error) {
 	if amount<=0{
@@ -50,35 +83,17 @@ func (s *Service) Pay(accountID int64, amount types.Money, category types.Paymen
 	return payment, nil
 }
 
-
-
-func (s *Service) FindAccountByID(accountID int64)(*types.Account, error)  {
+func (s *Service) FindAccountByID(accountID int64) (*types.Account, error)  {
+	
 	var account *types.Account
 	for _, acc := range s.accounts {
-		if acc.ID ==  accountID{       // платёж найден
-			account = acc	
-		}
-	}	
-
-	if account == nil {    // платёж не найден
-	return nil, ErrAccountNotFound
+		if acc.ID==accountID{
+			account=acc
+			break
+		}		
 	}
-	
-	return account, nil
-}
-func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
-	for _, account := range s.accounts {
-		if account.Phone == phone {
-			return nil, ErrAccountNotFound
-		}
+	if account==nil{
+		return nil, ErrAccountNotFound
 	}
-	s.nextAccountID++
-	account := &types.Account{
-		ID:      s.nextAccountID,
-		Phone:   phone,
-		Balance: 0,
-	}
-	s.accounts = append(s.accounts, account)
-	return account, nil
-
+	return account,nil
 }
